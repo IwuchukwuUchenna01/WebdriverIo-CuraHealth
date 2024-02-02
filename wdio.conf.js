d9a363390e5469d60671ac815c77049cc4aebee9
uchenna Iwuchukwu <oreoluwa371@gmail.com>
2024-02-02 12:18:37 +0100

    fixed config file

--- wdio.conf.js
+++ wdio.conf.js
@@ -53,7 +53,12 @@ exports.config = {
     //
     capabilities: [{
         // capabilities for local browser web tests
-        browserName: 'chrome' // or "firefox", "microsoftedge", "safari"
+        maxInstances: 5,
+        browserName: 'chrome',
+        acceptInsecureCerts: true,
+        'goog:chromeOptions': { 
+            args: ["--headless", "user-agent=...","--disable-gpu","--window-size=1440,735"]
+       }
     }],
 
     //
